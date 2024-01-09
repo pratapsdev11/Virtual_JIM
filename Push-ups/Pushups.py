89622994a36a2f9e4046f6fcbd2d555512d488f9
DivyaPratapSingh <109823056+DivyaPratapSinghjklu@users.noreply.github.com>
2024-01-09 18:30:49 +0530

    Add files via upload

---
+++ Push-ups/Pushups.py
@@ -0,0 +1,42 @@
+import cv2
+import mediapipe as md
+md_drawing=md.solutions.drawing_utils
+md_drawing_styles=md.solutions.drawing_styles
+md_pose=md.solutions.pose
+count=0
+position=None
+cap=cv2.VideoCapture(0) 
+with  md_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.7) as pose:
+    while cap.isOpened():
+        success,image=cap.read()
+        if not success:
+            print("empty camera")
+            break 
+        image=cv2.cvtColor(cv2.flip(image,1),cv2.COLOR_BGR2RGB)
+        result = pose.process(image)
+        imlist = []
+        if result.pose_landmarks:
+            md_drawing.draw_landmarks(
+              image, result.pose_landmarks, md_pose.POSE_CONNECTIONS)
+            for id,im in  enumerate(result.pose_landmarks.landmark):
+                h,w,c=image.shape
+                X,Y=int(im.x*w),int(im.y*h)
+                imlist.append([id,X,Y])
+            if len(imlist)!=0:
+                        if (imlist[12][2] and imlist[11][2] >= imlist[14][2] and imlist[13][2]):
+                            position = "down"
+                        if (imlist[12][2] and imlist[11][2] <= imlist[14][2] and imlist[13][2]) and position=="down":
+                            position = "up"
+                            count+=1
+                        print(count)
+        cv2.rectangle(image,(0,0),(225,73),(245,117,16),-1)
+        cv2.putText(image,'COUNT',(170,12),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1,cv2.LINE_AA)
+        cv2.putText(image,str(count),(200,40),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1,cv2.LINE_AA)             
+        cv2.imshow("Push-up counter",image)
+        key=cv2.waitKey(1)
+        if key ==ord("q"):
+            break
+cap.release()
+cv2.destroyAllWindows()     
